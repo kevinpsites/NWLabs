f72b92f6d7f633ceb0c5a698e64b63734202f0fc
Joshua-Haines <josh.smc@gmail.com>
2017-12-06 10:37:25 -0700

    Kevin's work on my stupid laptop. He hates my mouse and my computer

---
+++ NWLabs/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NWLabs.Models;
+
+namespace NWLabs.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private NWlabsContext db = new NWlabsContext();
+        static Customer Customer = new Customer();
+
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public ActionResult AddOrder()
+        {
+            return View();
+        }
+    }
+}
